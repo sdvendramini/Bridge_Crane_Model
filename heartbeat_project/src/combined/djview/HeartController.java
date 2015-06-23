6c90759eeb01612433cef46553bdacfda00614c6
joaquinfelici <joaquinfelici@users.noreply.github.com>
2015-06-23 11:21:58 -0300

    v2

--- heartbeat_project/src/combined/djview/HeartController.java
+++ heartbeat_project/src/combined/djview/HeartController.java
@@ -18,7 +18,7 @@ public class HeartController implements ControllerInterface {
 	public void stop() {}
     
 	public void increaseBPM() {
-		System.out.println("jajajajaj");
+		System.out.println("aloha");
 	}
     
 	public void decreaseBPM() {}
